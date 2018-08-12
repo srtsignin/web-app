c920b87f4b727a402163a790b6d68bb00b11964d
Connor Boyle <cjblink1@gmail.com>
2018-08-11 21:08:38 -0400

    Fixed student module reference

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StudentComponent } from './student/student.component';
+import { StudentComponent } from './student.component';
 
 @NgModule({
   imports: [
