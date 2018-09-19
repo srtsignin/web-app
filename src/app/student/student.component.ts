37d94ed951170d0c1f08f18394586cb2c9e1c0cc
Maya Holeman <holemanmaya@yahoo.com>
2018-09-19 12:29:41 -0400

    finished the student sign in

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,22 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../login/login.service';
+import { ActiveUsersService } from '../active-users.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css']
 })
-export class StudentComponent implements OnInit {
+export class StudentComponent implements OnInit {  
+  protected loggedIn: boolean;
+  protected courses : string[];
+  protected description : string;
 
-  courses : string[];
-
-  constructor() { }
+  constructor(private loginService: LoginService, private activeUsersService: ActiveUsersService) {
+    this.loginService.subscribe({
+      next: (loggedIn) => loggedIn ? this.loggedIn = true : this.loggedIn = false,
+    });
+  }
 
   ngOnInit() {
-    this.courses = ["", ""]
+    this.courses = [""]
   }
 
   addCourseRow() {
     this.courses.push("");
+  }
+
+  addStudent() {
+    let user = new User();
+    if (this.loggedIn) {
+       user.name = this.loginService.getFullName();
+    }
+    user.name = "Maya Holeman";
+    user.courses = this.courses.filter((v) => v != "");
+    user.problemDescription = this.description;
+    console.log(user);
+    this.activeUsersService.addUser(user);
   }
 
 }
--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { StudentComponent } from './student.component';
 import { MatButtonModule, MatCardModule, MatListModule, MatInputModule, MatGridListModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     MatCardModule,
     MatButtonModule,
     MatListModule,
