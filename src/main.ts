fcba817ca2c1dabe6f821a75f667f45455281b73
Maya Holeman <holemamk@rose-hulman.edu>
2018-10-29 22:10:12 -0400

    updated main to use default env

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,19 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { environmentLoader as environmentLoaderPromise } from './environments/environmentLoader';
 
-environmentLoaderPromise.then(env => {
-  if (env.production) {
-    enableProdMode();
-  }
-  environment.settings = env.settings;
+if (environment.production) {
+  enableProdMode();
+}
 
-  platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err));
-});
+
+// environmentLoaderPromise.then(env => {
+//   if (env.production) {
+//     enableProdMode();
+//   }
+//   environment.settings = env.settings;
+
+//   platformBrowserDynamic().bootstrapModule(AppModule)
+//     .catch(err => console.error(err));
+// });
